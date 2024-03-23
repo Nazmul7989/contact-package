05d35b425d2ac3428fcbfe314964024079c6eeac
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2024-03-23 10:56:43 +0600

    update service provider

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class ContactServiceProvider extends ServiceProvider
 {
-    public function register()
+    public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
         $this->loadViewsFrom(__DIR__.'/../resources/views', 'contact');
@@ -20,9 +20,10 @@ class ContactServiceProvider extends ServiceProvider
         ]);
     }
 
-    public function boot()
+    public function register()
     {
-
+        $this->mergeConfigFrom(__DIR__ . '/../config/contact.php', 'contact');
     }
+
 
 }
